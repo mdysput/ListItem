e547b9306489b80e7cf77db339cde26f20a5cc2a
Michal Dysput <michal.dysput@gmail.com>
2018-10-01 20:01:12 +0200

    Initial commit

---
+++ src/com/MD1/NodeList.java
@@ -0,0 +1,8 @@
+package com.MD1;
+
+public interface NodeList {
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem root);
+}
